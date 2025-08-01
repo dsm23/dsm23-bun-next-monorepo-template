511800a03bb1e1aeff39cd1696b8fedcadeb5688
David Murdoch <dsmurdoch357@gmail.com>
2025-08-01 19:13:15 +0100

    ci(renovate): add custom manager for ts in prettier configs

--- packages/ui/prettier.config.js
+++ packages/ui/prettier.config.js
@@ -25,7 +25,8 @@ const config = {
     "^(?!.*[.]css$)[./].*$",
     ".css$",
   ],
-  importOrderTypeScriptVersion: "5.7.3",
+  // renovate: datasource=npm depName=typescript
+  importOrderTypeScriptVersion: "5.9.2",
   tailwindFunctions: ["clsx", "cn", "cva", "cx"],
 };
 
